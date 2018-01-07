66006c7db782b41ed43e2cc45880468343925813
Konrad Tallman <konradt95@gmail.com>
2018-01-07 16:13:02 -0600

    Adds a function that attempts to read everything sent from a telnet server

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
-(defproject clj-telnet "0.1.0"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+(defproject clj-telnet "0.1.1"
+  :description "A clojure telnet client"
+  :url "https://github.com/komcrad/clj-telnet"
+  :license {:name "GPL-2.0"
+            :url "https://www.gnu.org/licenses/old-licenses/gpl-2.0.en.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [commons-net/commons-net "3.5"]])
